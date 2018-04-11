a881d5b5dcbfe60690d0a09981f48d34370ff755
jerry-an <jerryanj@hotmail.com>
2018-04-11 18:05:13 +0800

    update strategy chart

--- js/strat_crypto.js
+++ js/strat_crypto.js
@@ -1,104 +1,158 @@
-$(document).ready(function(){
-	$.stratlist = new Vue({
-		el: '#stratListCrypto',
-		data: {
-			global: {},
-			stratReports:[
-			{
-				'strat-name': 'CoAlpha Strategy',
-				'industry-name': 'CoAlpha Inc.',
-				'description': 'The CoAlpha MOM30 index product is a cryptocurrency investment product that adopts index strategy. This index strategy is a simple quantitative strategy that dynamically adjusts the portfolio weights of 30 cryptocurrency based on market capital and momentum. It can achieve a higher return to risk ratio  than holding a single digital currency such as BTC, due to investment diversification and dynamic allocation.  For 2017, this strategy has a return of 17070% with Sharpe Ratio 4.82 and Sortino Ratio 7.12, versus BTC\'s return of 1384% with Sharpe Ratio 3.11 and Sortino Ratio 4.55. The advantages for this strategy are: better diversification, easy to understand, easy to implement the trade execution, and large capital capacity. So it is suitable for large scale asset allocation and less sophisticated investors.',
-				'stock-list-list': 
-				{
-				 "top": [
-				  
-				 ],
-				 "bottom": [
-				 ]
-				}
-			},
-			
-			/*, append strat according to above
-			{
-				'strat-name': 'CoAlpha Strategy',
-				'industry-name': 'CoAlpha Inc.',
-			}
-			*/
-			]
-		},
-		computed: {
-				hasList : function() {
-					return (this.stratReports['stock-list-list'] != undefined)
-				}
-
-		},
-		methods: {
-			uiStratList: function(idx) {
-				if (this.stratReports[idx]['stock-list-list'] === undefined)
-					return []
-				var tmp = _.zip(this.stratReports[idx]['stock-list-list']['top'],
-					this.stratReports[idx]['stock-list-list']['bottom'])
-				console.log(tmp)
-				return tmp
-			}
-		}
-		
-	});
-	var myChart = echarts.init(document.getElementById('Strategy-img-fluid-1'));
-	$.get('js/crypto_nav.json', function (data) {
-	    myChart.setOption(option = {
-	        title : {
-               subtext: 'MOM30Index:sharpe=4.82;sortino=7.12\nBTC:sharpe=3.11;sortino=4.55'
-            },
-	        legend: {
-
-               data:['MOM30Index','BTC'],
-               top: 40
-            },
-	        tooltip: {
-	            trigger: 'axis'
-	        },
-	        xAxis: {
-	        	axisLabel : {
-                     formatter: function(category){
-                           return category.substring(0,6);
-                          }
+$(document).ready(function() {
+    $.stratlist = new Vue({
+        el: '#stratListCrypto',
+        data: {
+            global: {},
+            stratReports: [{
+                'strat-name': 'CoAlpha Strategy(based on USD) ',
+                'industry-name': 'CoAlpha Inc.',
+                'description': 'The CoAlpha MOM30 index product is a cryptocurrency investment product that adopts index strategy. This index strategy is a simple quantitative strategy that dynamically adjusts the portfolio weights of 30 cryptocurrency based on market capital and momentum. It can achieve a higher return to risk ratio  than holding a single digital currency such as BTC, due to investment diversification and dynamic allocation.  In last year, this strategy has a return of 5418% with Sharpe Ratio 4.82 and Sortino Ratio 7.12, versus ETH\'s return of 129% with Sharpe Ratio 0.74 and Sortino Ratio 1.04. The advantages for this strategy are: better diversification, easy to understand, easy to implement the trade execution, and large capital capacity. So it is suitable for large scale asset allocation and less sophisticated investors.',
+                'stock-list-list': {
+                    "top": [],
+                    "bottom": []
                 },
-	            data: data.map(function (item) {
-	                return item[0];
-	            })
-	        },
-	        yAxis: {
-	        	title: 'NAV',
-	            splitLine: {
-	                show: false
-	            }
-	        },
-	        toolbox: {
-	            left: 'center',
-	            feature: {
-	                dataZoom: {
-	                    yAxisIndex: 'none'
-	                },
-	                restore: {},
-	                saveAsImage: {}
-	            }
-	        },
-	        series: [
-	        {
-	            name: 'MOM30Index',
-	            type: 'line',
-	            data: data.map(function (item) {
-	                return item[1];
-	            }),
-	        },
-	        {
-	            name: 'BTC',
-	            type: 'line',
-	            data: data.map(function (item) {
-	                return item[2];
-	            }),
-	        }]
-    	});
-	});
+                'imgFluid': 'Strategy-img-fluid-1'
+            }, {
+                'strat-name': 'CoAlpha Strategy(based on ETH)',
+                'industry-name': 'CoAlpha Inc.',
+                'description': 'This product is based on ETH. In last year, this strategy has a return of 2674% with Sharpe Ratio 3.54 and Sortino Ratio 4.55, versus BTC\'s return of 78% with Sharpe Ratio 0.35 and Sortino Ratio 0.43.',
+                'stock-list-list': {
+                    "top": [],
+                    "bottom": []
+                },
+                'imgFluid': 'Strategy-img-fluid-2'
+            }]
+        },
+        computed: {
+            hasList: function() {
+                return (this.stratReports['stock-list-list'] != undefined)
+            }
+
+        },
+        methods: {
+            uiStratList: function(idx) {
+                if (this.stratReports[idx]['stock-list-list'] === undefined)
+                    return []
+                var tmp = _.zip(this.stratReports[idx]['stock-list-list']['top'],
+                    this.stratReports[idx]['stock-list-list']['bottom'])
+                console.log(tmp)
+                return tmp
+            }
+        }
+
+    });
+    var usdChart = echarts.init(document.getElementById('Strategy-img-fluid-1'));
+    $.get('js/crypto_btc.json', function(data) {
+        usdChart.setOption(option = {
+            title: {
+                subtext: 'MOM30Index:sharpe=4.95;sortino=5.87\nETH:sharpe=0.74;sortino=1.04'
+            },
+            legend: {
+
+                data: ['MOM30Index', 'ETH'],
+                top: 40
+            },
+            tooltip: {
+                trigger: 'axis'
+            },
+            xAxis: {
+                axisLabel: {
+                    formatter: function(category) {
+                        return category.substring(4, 8);
+                    }
+                },
+                data: data.map(function(item) {
+                    return item[0];
+                })
+            },
+            yAxis: {
+                title: 'NAV',
+                splitLine: {
+                    show: false
+                }
+            },
+            toolbox: {
+                left: 'center',
+                feature: {
+                    dataZoom: {
+                        yAxisIndex: 'none'
+                    },
+                    restore: {},
+                    saveAsImage: {}
+                }
+            },
+            series: [{
+                    name: 'MOM30Index',
+                    type: 'line',
+                    data: data.map(function(item) {
+                        return item[1];
+                    }),
+                },
+                {
+                    name: 'ETH',
+                    type: 'line',
+                    data: data.map(function(item) {
+                        return item[2];
+                    }),
+                }
+            ]
+        });
+    });
+    var ethChart = echarts.init(document.getElementById('Strategy-img-fluid-2'));
+    $.get('js/crypto_eth.json', function(data) {
+        ethChart.setOption(option = {
+            title: {
+                subtext: 'MOM30Index:sharpe=3.54;sortino=4.55\nBTC:sharpe=0.35;sortino=0.43'
+            },
+            legend: {
+
+                data: ['MOM30Index', 'BTC'],
+                top: 40
+            },
+            tooltip: {
+                trigger: 'axis'
+            },
+            xAxis: {
+                axisLabel: {
+                    formatter: function(category) {
+                        return category.substring(4, 8);
+                    }
+                },
+                data: data.map(function(item) {
+                    return item[0];
+                })
+            },
+            yAxis: {
+                title: 'NAV',
+                splitLine: {
+                    show: false
+                }
+            },
+            toolbox: {
+                left: 'center',
+                feature: {
+                    dataZoom: {
+                        yAxisIndex: 'none'
+                    },
+                    restore: {},
+                    saveAsImage: {}
+                }
+            },
+            series: [{
+                    name: 'MOM30Index',
+                    type: 'line',
+                    data: data.map(function(item) {
+                        return item[1];
+                    }),
+                },
+                {
+                    name: 'BTC',
+                    type: 'line',
+                    data: data.map(function(item) {
+                        return item[2];
+                    }),
+                }
+            ]
+        });
+    });
 });
--- rawdata/crypto_nav.py
+++ rawdata/crypto_nav.py
@@ -2,6 +2,26 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import datetime as dt
 import json
-df = pd.read_csv('crypto_index.csv')
+#df = pd.read_csv('crypto_usd.csv')
+#data = {}
+#ccindex = [(x, y, z) for x, y, z in zip(df['Date'].apply(lambda x:x.replace('-','')), map(lambda x:"%.2f" % x, df['Index']), map(lambda x:"%.2f" % x, df['BTC']))]
+#print json.dumps(ccindex)
+
+df = pd.read_csv('crypto_btc.csv')
 data = {}
-print json.dumps([(x, y, z) for x, y, z in zip(df['Date'].apply(lambda x:x.replace('-','')), map(lambda x:"%.2f" % x, df['Index']), map(lambda x:"%.2f" % x, df['BTC']))])
+ccindex = [(x, y, z) for x, y, z in zip(df['Date'], map(lambda x:"%.2f" % x, df['Index']), map(lambda x:"%.2f" % x, df['ETH/BTC']))]
+#print json.dumps(ccindex)
+with open("crypto_btc.json","w") as f:
+	json.dump(ccindex,f)
+
+df = pd.read_csv('crypto_eth.csv')
+data = {}
+ccindex = [(x, y, z) for x, y, z in zip(df['Date'], map(lambda x:"%.2f" % x, df['Index']), map(lambda x:"%.2f" % x, df['BTC/ETH']))]
+#print json.dumps(ccindex)
+
+with open("crypto_eth.json","w") as f:
+	json.dump(ccindex,f)
+
+#df = pd.read_csv('crypto_usd.csv')
+#data = {}
+#print json.dumps([(x, y, z) for x, y, z in zip(df['Date'].apply(lambda x:x.replace('-','')), map(lambda x:"%.2f" % x, df['Index']), map(lambda x:"%.2f" % x, df['BTC/ETH']))])
