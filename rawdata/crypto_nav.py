4f2bdd2a1b14b3202874c718cd76150d72481e76
jerry-an <jerryanj@hotmail.com>
2018-04-17 13:43:15 +0800

    update

--- rawdata/crypto_nav.py
+++ rawdata/crypto_nav.py
@@ -3,26 +3,23 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import datetime as dt
 import json
+import sys
 #df = pd.read_csv('crypto_usd.csv')
 #data = {}
 #ccindex = [(x, y, z) for x, y, z in zip(df['Date'].apply(lambda x:x.replace('-','')), map(lambda x:"%.2f" % x, df['Index']), map(lambda x:"%.2f" % x, df['BTC']))]
 #print(json.dumps(ccindex))
 
-df = pd.read_csv('crypto_btc.csv')
+
+filename = sys.argv[1]
+strategy = sys.argv[2]
+filedir = '/data/analysis/ljs/sim/CC/hour/'
+
+output = {"btc":"crypto_btc.json","eth":"crypto_eth.csv","usd":"crypto_usd.csv"}
+benchmark = {"btc":"ETH/BTC","eth":"BTC/ETH","usd":"BTC/USD"}
+
+df = pd.read_csv(filedir + filename)
 data = {}
-ccindex = [(x, y, z) for x, y, z in zip(df['Date'], map(lambda x:"%.2f" % x, df['Index']), map(lambda x:"%.2f" % x, df['ETH/BTC']))]
+ccindex = [(x, y, z) for x, y, z in zip(df['Date'], map(lambda x:"%.2f" % x, df['Index']), map(lambda x:"%.2f" % x, df[benchmark[strategy]]))]
 #print json.dumps(ccindex)
-with open("crypto_btc.json","w") as f:
+with open(output[strategy],"w") as f:
 	json.dump(ccindex,f)
-
-df = pd.read_csv('crypto_eth.csv')
-data = {}
-ccindex = [(x, y, z) for x, y, z in zip(df['Date'], map(lambda x:"%.2f" % x, df['Index']), map(lambda x:"%.2f" % x, df['BTC/ETH']))]
-#print(json.dumps(ccindex))
-
-with open("crypto_eth.json","w") as f:
-	json.dump(ccindex,f)
-
-#df = pd.read_csv('crypto_usd.csv')
-#data = {}
-#print json.dumps([(x, y, z) for x, y, z in zip(df['Date'].apply(lambda x:x.replace('-','')), map(lambda x:"%.2f" % x, df['Index']), map(lambda x:"%.2f" % x, df['BTC/ETH']))])
